a41fa3cdeac994d1f01d9666daa712428b355204
sadaf3 <sadafsultan3@gmail.com>
2017-04-09 18:25:09 +0530

    grid view working

--- app/src/main/java/com/example/root/sohaari/Main2Activity.java
+++ app/src/main/java/com/example/root/sohaari/Main2Activity.java
@@ -2,12 +2,15 @@ package com.example.root.sohaari;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    GridView gv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        gv1=(GridView)findViewById(R.id.gv);
+        gv1.setAdapter(new ImageAdapter(this));
     }
 }
